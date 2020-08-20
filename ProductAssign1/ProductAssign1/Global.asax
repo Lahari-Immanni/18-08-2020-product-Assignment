2c12cc4dbbc8d647469d7a7255f7973c3ab2ead9
lahari <lahari.immanni@zensar.com>
2020-08-20 13:58:11 -0700

    Product Assignment

---
+++ ProductAssign1/ProductAssign1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProductAssign1.MvcApplication" Language="C#" %>
---
+++ ProductAssign1/ProductAssign1/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductAssign1.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public String ProductName { get; set; }
+        public double Rate { get; set; }
+    }
+}
